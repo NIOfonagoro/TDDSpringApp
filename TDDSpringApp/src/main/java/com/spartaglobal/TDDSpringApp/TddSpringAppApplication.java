c811a6abd2b704ee861acba8f1daf0ff85e1293c
Nicholas Ofonagoro <nofonagoro@spartaglobal.com>
2019-05-28 10:53:56 +0100

    Initial Commit

---
+++ TDDSpringApp/src/main/java/com/spartaglobal/TDDSpringApp/TddSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.spartaglobal.TDDSpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddSpringAppApplication.class, args);
+	}
+
+}
---
+++ TDDSpringApp/src/test/java/com/spartaglobal/TDDSpringApp/TddSpringAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spartaglobal.TDDSpringApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TddSpringAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
